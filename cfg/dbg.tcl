b4376ebc179cda40f62bbd04648fdbea9eaec543
hz <dxp@ngs.ru>
2017-06-15 19:42:56 +0700

    rename debug console commands

--- cfg/dbg.tcl
+++ cfg/dbg.tcl
@@ -1,15 +1,19 @@
+# connect
 proc cn { } {
     connect -host localhost -port 3121
     targets -set -filter {name =~ "ARM* #0"}
 }
 
+# reload
 proc rl { } {
     rst
     dow bin/fsbl.elf
     con 
 }
 
-proc rlc { } {
+# restart
+proc rs { } {
     rl
     con 
 }
+
